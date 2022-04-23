4978fae3d0f8020ff1dca2d2d271066953c773cd
duyquang2301 <duyquang.tdq7@gmail.com>
2022-04-23 14:23:27 +0700

    Cart

---
+++ PBL3/public/js/Cart.js
@@ -0,0 +1,13 @@
+﻿const minusProduct = (e) => {
+    let input = e.parentNode.nextElementSibling;
+    let value = input.value;
+    if (value > 1) {
+        input.value = parseInt(value) - 1;
+    }
+}
+
+const plusProduct = (e) => {
+    let input = e.parentNode.previousElementSibling;
+    let value = input.value;
+    input.value = parseInt(value) + 1;
+}
