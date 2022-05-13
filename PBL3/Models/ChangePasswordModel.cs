5c2bae1542271b750a715260267329c34bb8ca82
duyquang2301 <duyquang.tdq7@gmail.com>
2022-05-13 20:56:52 +0700

    User

---
+++ PBL3/Models/ChangePasswordModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PBL3.Models
+{
+    public class ChangePasswordModel
+    {
+        [Required]
+        public int UserID { get; set; }
+        [Required(ErrorMessage = "Mật khẩu hiện tại không được để trống")]
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Mật khẩu không chứa kí tự đặc biệt")]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "Mật khẩu có độ dài từ 8 - 32")]
+        public string OldPassword { get; set; }
+        [Required(ErrorMessage = "Mật khẩu mới không được để trống")]
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Mật khẩu không chứa kí tự đặc biệt")]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "Mật khẩu có độ dài từ 8 - 32")]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Mật khẩu xác nhận không được để trống")]
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Mật khẩu không chứa kí tự đặc biệt")]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "Mật khẩu có độ dài từ 8 - 32")]
+        public string ConfirmPassword { get; set; }
+    }
+}
