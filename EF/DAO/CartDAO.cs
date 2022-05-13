a51ffba81cc094f0cbb7f31c5c6a051f8b7ec34e
= <=>
2022-05-13 16:10:26 +0700

    Update + Delete + Show Cart

--- EF/DAO/CartDAO.cs
+++ EF/DAO/CartDAO.cs
@@ -1,6 +1,7 @@
 ﻿using EF.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,20 @@ namespace EF.DAO
         {
             context = new ShopOnlineDbContext();
         }
-        public bool add_Update(int id, int quantity, int CartID, bool isUpdate = false)
+        public List<CartProduct> GetProductCart(int CartID)
+        {
+            return context.CartProduct.AsNoTracking().Select(cp => new CartProduct
+            {
+                ID = cp.ID,
+                CartID = cp.CartID,
+                ProductID = cp.ProductID,
+                Status = cp.Status,
+                Quantity = cp.Quantity,
+                InsertedAt = cp.InsertedAt,
+                Product = context.Products.FirstOrDefault(p => p.ID == cp.ProductID)
+            }).Where(cp => cp.CartID == CartID).ToList();
+        }
+        public bool add_Update(int id, int quantity, int CartID)
         {
             Cart cart = context.Carts.Find(CartID);
             Product product = context.Products.Find(id);
@@ -23,33 +37,49 @@ namespace EF.DAO
                 CartProduct productInCart = context.CartProduct.Where(e => e.CartID == CartID && e.ProductID == id).FirstOrDefault();
                 if (productInCart != null)
                 {
-                    if (isUpdate)
-                    {
-                        productInCart.Quantity = quantity;
-                    }
-                    else
+                    if (productInCart.Quantity + quantity <= product.Stock)
                     {
                         productInCart.Quantity += quantity;
+                        productInCart.UpdatedAt = DateTime.Now;
+                        context.SaveChanges();
+                        return true;
                     }
-                    productInCart.UpdatedAt = DateTime.Now;
                 }
                 else
                 {
-                    CartProduct cartProduct = new CartProduct
+                    if(quantity <= product.Stock)
                     {
-                        CartID = CartID,
-                        ProductID = id,
-                        Quantity = quantity,
-                        Status = true,
-                        InsertedAt = DateTime.Now,
-                        UpdatedAt = null
-                    };
-                    context.CartProduct.Add(cartProduct);
+                        CartProduct cartProduct = new CartProduct
+                        {
+                            CartID = CartID,
+                            ProductID = id,
+                            Quantity = quantity,
+                            Status = true,
+                            InsertedAt = DateTime.Now,
+                            UpdatedAt = null
+                        };
+                        context.CartProduct.Add(cartProduct);
+                        context.SaveChanges();
+                        return true;
+                    } 
                 }
+                return false;
+            }
+            return false;
+        }
+        public bool DeleteProduct(int id)
+        {
+            CartProduct cartProduct = context.CartProduct.Find(id);
+            if (cartProduct != null)
+            {
+                context.Remove(cartProduct);
                 context.SaveChanges();
                 return true;
             }
-            return false;
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- PBL3/public/js/Cart.js
+++ PBL3/public/js/Cart.js
@@ -1,14 +1,1 @@
-﻿const minusProduct = (e) => {
-    let input = e.parentNode.nextElementSibling;
-    let value = input.value;
-    if (value > 1) {
-        input.value = parseInt(value) - 1;
-    }
-}
-
-
-const plusProduct = (e) => {
-    let input = e.parentNode.previousElementSibling;
-    let value = input.value;
-    input.value = parseInt(value) + 1;
-}
+﻿
