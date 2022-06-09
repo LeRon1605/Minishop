1c6ee2ee2d975f4db21e70bf8526b276e0984e78
duyquang2301 <duyquang.tdq7@gmail.com>
2022-06-09 11:03:46 +0700

    Update, Delete ImportBill in BO, Controller

--- PBL3/Areas/Admin/Controllers/ImportBillController.cs
+++ PBL3/Areas/Admin/Controllers/ImportBillController.cs
@@ -1,4 +1,5 @@
-﻿using Models.DTO;
+﻿using Models.BLL;
+using Models.DTO;
 using PBL3.Helper;
 using System;
 using System.Collections.Generic;
@@ -19,14 +20,55 @@ namespace PBL3.Areas.Admin.Controllers
 
         public ActionResult Update(ImportBill bill)
         {
-            // 
-            return null;
+            if(ModelState.IsValid)
+            {
+                ImportBillBO importBillBO = new ImportBillBO();
+                if(importBillBO.Update(bill) == true)
+                {
+                    TempData["Status"] = true;
+                    TempData["Message"] = "Cập nhật hóa đơn thành công";
+                }    
+                else
+                {
+                    TempData["Status"] = true;
+                    TempData["Message"] = "Cập nhật hóa đơn thất bại";
+                }
+            }
+            else
+            {
+                TempData["Status"] = false;
+                TempData["Message"] = "Cập nhật hóa đơn thất bại";
+               
+            }
+            return RedirectToAction("Index");
         }
 
         public ActionResult Delete(int id)
         {
-            // 
-            return null;
+            ImportBillBO importBillBO = new ImportBillBO();
+            if (importBillBO.Delete(id))
+            {
+                return new JsonResult
+                {
+                    Data = new
+                    {
+                        message = "Xóa thành công hóa hàng",
+                        status = true
+                    }
+                };
+            }
+            else
+            {
+                return new JsonResult
+                {
+                    Data = new
+                    {
+                        message = "Xóa hóa đơn thất bại",
+                        detail = "Hóa đơn không tồn tại",
+                        status = true
+                    }
+                };
+            }
         }
 
     }
